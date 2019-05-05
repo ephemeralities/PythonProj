790f528fd82159ac615e41234ef8de5d2947ef04
Diego Martinez <itsmeyeyo@gmail.com>
2019-05-05 16:59:05 -0500

    Update serial.py

--- serial.py
+++ serial.py
@@ -6,43 +6,67 @@ from collections import deque
 import pygame
 import serial
 
-screen = pygame.display.set_mode((300, 300))
-pygame.display.flip()
+#Necessary state variables
+PORT = "INSERT PORT HERE"
+ARDUINO_AVAILABLE = True
+running = True
+mouse_down = False
+prev_point = 0
+new_point = 0
+gap_offset = 10
 
+screen = pygame.display.set_mode((300, 300))
 screen.fill((255, 255, 255))
 
 surface = pygame.display.get_surface()
-
-gap_offset = 10
-
 pygame.display.flip()
 
-running = True
-mouse_down = False
-
-prev_point = 0
-new_point = 0
-
+#Queue for storing points. FILO (First in, Last out) data structure
 point_queue = deque()
 
+#Starts a new thread; Send_Serial is the function the thread runs
 thread = threading.Thread(target=send_serial)
 
-arduino = serial.Serial('COM7', 19200, write_timeout = 3)
+#Creates new Serial object: Used for communicating with main Arduino
+arduino = serial.Serial(PORT, 9600, timeout = 1, write_timeout = 3)
 
 def send_serial():
     global point_queue
     buffer = 0
     
-    while True:
-        if len(point_queue) > 0 or buffer < 64:
-            points = point_queue.popleft()
-            points_str = ""
-            for point in points:
-                arduino.write(struct.pack('>b', point))
-        else:
-            time.sleep(0.5)
+    prev_response = None
 
+    while True:
+
+        arduino_response = arduino.read()
+
+        try:
+            arduino_response = int.from_bytes(arduino_response, 'little')
+        except:
+            #failsafe in the case that the Arduino hasn't given a response (maybe stuck calculating)
+            arduino_response = prev_response
+
+        if arduino_response == 1:
+            ARDUINO_AVAILABLE = False
+        else:
+            ARDUINO_AVAILABLE = True
+
+        if not ARDUINO_AVAILABLE:
+            time.sleep(0.1)
+        else:
+            if len(point_queue) > 0 or buffer < 64:
+                points = point_queue.popleft()
+                for point in points:
+                    #Since numbers are objects in python, this convert it into an unsigned char (1 byte, little endian)
+                    arduino.write(struct.pack('<B', point))
+            else:
+                time.sleep(0.1)
+
+        prev_response = arduino_response
+
+#Starts Serial write thread
 thread.start()
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONUP:
