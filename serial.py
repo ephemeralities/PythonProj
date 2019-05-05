c48721e011ecac9097d1ae554c67e6485e72c95e
Diego Martinez <itsmeyeyo@gmail.com>
2019-05-05 01:08:46 -0500

    Update serial.py

--- serial.py
+++ serial.py
@@ -1,5 +1,10 @@
+import struct
+import threading
+import time
 from collections import deque
-import pygame, serial
+
+import pygame
+import serial
 
 screen = pygame.display.set_mode((300, 300))
 pygame.display.flip()
@@ -20,9 +25,24 @@ new_point = 0
 
 point_queue = deque()
 
+thread = threading.Thread(target=send_serial)
 
-arduino = serial.Serial('COM3', 19200, write_timeout = 3)
+arduino = serial.Serial('COM7', 19200, write_timeout = 3)
 
+def send_serial():
+    global point_queue
+    buffer = 0
+    
+    while True:
+        if len(point_queue) > 0 or buffer < 64:
+            points = point_queue.popleft()
+            points_str = ""
+            for point in points:
+                arduino.write(struct.pack('>b', point))
+        else:
+            time.sleep(0.5)
+
+thread.start()
 while running:
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONUP:
@@ -40,14 +60,14 @@ while running:
 
             if (new_x > old_x + gap_offset or new_x < old_x - gap_offset) or (new_y > old_y + gap_offset or new_y < old_y - gap_offset):
                 new_point = (event.pos[0], event.pos[1])
-                point = str(new_point[0]) + str(new_point[1)]
+                point = str(new_point[0]) + str(new_point[1])
                 point_queue.append(point)
                 
                 pygame.draw.line(surface, (0,0,0), prev_point, new_point, 5)
                 pygame.display.flip()
             
         if event.type == pygame.QUIT:
+            thread.end()
             print(event)
             pygame.display.quit()
-            pygame.quit()
-            
+pygame.quit()
